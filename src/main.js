e6e2f8eaeed1c1c524c871a1494ab3d29059bd65
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-24 00:04:10 +0300

    made app core

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import subscriber from './plugins/subscriber'
 Vue.config.productionTip = false
+Vue.use(subscriber);
 
-new Vue({
+window.$vue = new Vue({
   router,
   store,
   render: h => h(App)
