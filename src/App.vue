9b01d4e292905a24b5dbe61b603580eb4ace52b0
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-24 00:04:18 +0300

    made app container

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<div id="app">
+  <div id="nav">
+    <router-link to="/">Courses</router-link> |
+    <router-link to="/settings">Settings</router-link>
   </div>
+  <router-view />
+</div>
 </template>
 
 <style lang="scss">
+body,
+html {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  overflow: hidden;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 #nav {
-  padding: 30px;
+  background-color: #f8f8f8;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    margin: 0 5px;
 
     &.router-link-exact-active {
       color: #42b983;
